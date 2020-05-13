23056b64ddc46ca56afecd9f47c78d76d92da1a2
Spyryls <yr410ad@gmail.com>
2020-05-13 16:11:29 -0500

    Blows up at Pageable

--- src/main/java/com/jasonvanblarcum/showcase/services/UserServiceStubImpl.java
+++ src/main/java/com/jasonvanblarcum/showcase/services/UserServiceStubImpl.java
@@ -1,13 +1,41 @@
 package com.jasonvanblarcum.showcase.services;
 
+import com.jasonvanblarcum.showcase.models.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
 public class UserServiceStubImpl implements UserService {
-    @Override
+
+
     public boolean authenticate(String username, String password) {
         return Objects.equals(username, password);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public User findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public User create(User user) {
+        return null;
+    }
+
+    @Override
+    public User edit(User user) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
     }
 }
